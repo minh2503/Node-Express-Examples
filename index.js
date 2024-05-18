8156da9963a975e7de665a067768c1790aa5b804
Minh <0942051066minh@gmail.com>
2024-05-18 10:57:33 +0700

    Express Router

---
+++ index.js
@@ -0,0 +1,73 @@
+const { log } = require('console');
+const express = require('express')
+http = require('http')
+const hostname = 'localhost'
+const post = 3001
+//morgan
+const morgan = require('morgan');
+//bodeParser
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use((req, res, next) => {
+    console.log(req.headers)
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is a an Express Server</h1></body></html>');
+
+})
+app.use(morgan('dev'));
+
+//ex5
+app.use(bodyParser.json());
+
+app.all('/dishes', (req,res,next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next();
+});
+
+app.get('/dishes', (req,res,next) => {
+    res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+ res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+});
+ 
+app.delete('/dishes', (req, res, next) => {
+    res.end('Deleting all dishes');
+});
+
+app.get('/dishes/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/'+ req.params.dishId);
+});
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + 
+        ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+//Router
+const dishRouter = require('./routes/dishRouter');
+
+app.use('/dishes', dishRouter);
+
+app.use(express.static(__dirname + '/public'));
+const server = http.createServer(app);
+server.listen(3001, hostname, () => {
+    console.log(`Server running at http://${hostname}:${3001}/`);
+})
